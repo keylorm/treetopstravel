9a1605b8284bffd9260f25b91266b37c78212e46
Keylor Mora <keylor@orbelink.com>
2015-05-07 17:38:57 -0600

    slider

--- sites/all/themes/treetopstravel/templates/fields/field--field-slides.tpl.php
+++ sites/all/themes/treetopstravel/templates/fields/field--field-slides.tpl.php
@@ -1,8 +1,8 @@
 
 <?php
 if ($items){
+
 $imagen ="";
-dpm($items);
 ?>
 <!-- Place somewhere in the <body> of your page -->
 <?php for ($i=0; $i < count($items); $i++) {
@@ -35,16 +35,7 @@ dpm($items);
 			
 
 		}
-		/*if($i==(count($items)-1)){
-			$last="logo-pasamano-last";
-		}else{
-		$last="";
-		}
-		if($i==0){
-			$first="logo-pasamano-first";
-		}else{
-		$first="";
-		}*/
+		
 		if($url != ''){
 			if($link != ''){
 				if ($titulo_slide != '' || $desc_slide != ''){
@@ -56,7 +47,7 @@ dpm($items);
 					}
 					
 
-					$imagen .= "<li><a href=\"".$link."\" ><img src=\"/sites/default/files/slides/".$items[$i]['field_image']['#items'][0]['filename']."\" /></a><div class='caption'>".$titulo_slide_markup.$desc_slide_markup."</div></li>";
+					$imagen .= "<li><a href=\"".$link."\" ><img src=\"/sites/default/files/slides/".$items[$i]['field_image']['#items'][0]['filename']."\" /></a><div class='flex-caption'>".$titulo_slide_markup.$desc_slide_markup."</div></li>";
 				}else{
 					$imagen .= "<li><a href=\"".$link."\" ><img src=\"/sites/default/files/slides/".$items[$i]['field_image']['#items'][0]['filename']."\" /></a></li>";
 				}
@@ -70,17 +61,13 @@ dpm($items);
 					}
 					
 
-					$imagen .= "<li><img src=\"/sites/default/files/slides/".$items[$i]['field_image']['#items'][0]['filename']."\" /><div class='caption'>".$titulo_slide_markup.$desc_slide_markup."</div></li>";
+					$imagen .= "<li><img src=\"/sites/default/files/slides/".$items[$i]['field_image']['#items'][0]['filename']."\" /><div class='flex-caption'>".$titulo_slide_markup.$desc_slide_markup."</div></li>";
 				}else{
 					$imagen .= "<li><img src=\"/sites/default/files/slides/".$items[$i]['field_image']['#items'][0]['filename']."\" /></li>";
 			}
 
 			
-		}/*else{
-			$imagen .= "<div class=\"logo-pasamano logo-pasamano-".($i+1)." ".$first." ".$last."\">
-			<div class='logo-pasamanos-imagen'><img src=\"/sites/default/files/listado_pasamanos/".$items[$i]['field_imagen_thumbnail']['#items'][0]['filename']."\" /></div>
-			<div class='logo-pasamanos-nombre'><p>".$items[$i]['field_nombre_pasamano']['#items'][0]['value']."</p></div>
-			</div>";*/
+		}
 	}
 } ?>
 <div class="flexslider">
@@ -88,4 +75,4 @@ dpm($items);
 		<?php print $imagen ?>
 	</ul>
 </div>
-<?php } ?>
+<?php 	flexslider_add(); } ?>
