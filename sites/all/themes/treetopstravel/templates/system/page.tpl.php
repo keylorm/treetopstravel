54a93bd67016e3aebd58495ff8505ad581b9b080
Keylor Mora <keylor@orbelink.com>
2015-05-21 14:43:17 -0600

    grid-1000 h1

--- sites/all/themes/treetopstravel/templates/system/page.tpl.php
+++ sites/all/themes/treetopstravel/templates/system/page.tpl.php
@@ -83,9 +83,10 @@
 
     <a id="main-content"></a>
     <div class="main" role="main">
-      <?php print render($title_prefix); ?>
+      <div class="grid-1000"><?php print render($title_prefix); ?>
       <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
       <?php print render($title_suffix); ?>
+    </div>
 
       <?php if ($tabs): ?>
         <?php print render($tabs); ?>
