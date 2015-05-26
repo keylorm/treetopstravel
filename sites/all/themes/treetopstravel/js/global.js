f6218f96dee0d42ade85f622104f2da50c633206
asv86 <andreys2009@gmail.com>
2015-05-26 16:03:36 -0600

    Css

--- sites/all/themes/treetopstravel/js/global.js
+++ sites/all/themes/treetopstravel/js/global.js
@@ -15,3 +15,19 @@ jQuery(window).load(function() {
     directionNav: true
   });
 });
+
+// Controlled by buttons
+var $controlledByButtons = $('#controlledByButtons');
+$controlledByButtons.updown({
+	step: 10,
+	shiftStep: 100
+});
+var $updown = $controlledByButtons.data('updown');
+$('#btnIncrease').click(function(event){
+	$updown.increase(event);
+	$updown.triggerEvents();
+});
+$('#btnDecrease').click(function(event){
+	$updown.decrease(event);
+	$updown.triggerEvents();
+});
