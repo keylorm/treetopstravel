c7ed822e30d93bdcf39eb5834825ddf56698d793
Keylor Mora <keylor@orbelink.com>
2015-05-28 09:10:44 -0600

    atracciones

--- sites/all/themes/treetopstravel/js/global.js
+++ sites/all/themes/treetopstravel/js/global.js
@@ -23,6 +23,13 @@ jQuery(document).ready(function() {
   jQuery(".i18n-en .views-exposed-form .form-item-field-destino-target-id select option[value='All']").text("All the destinations");    
 
 });
+jQuery(document).ajaxSuccess(function() {
+  jQuery(".i18n-es .views-exposed-form .form-item-field-region-tid select option[value='All']").text("Todas las regiones");
+  jQuery(".i18n-es .views-exposed-form .form-item-field-destino-target-id select option[value='All']").text("Todos los destinos");    
+  jQuery(".i18n-en .views-exposed-form .form-item-field-region-tid select option[value='All']").text("All the regions");
+  jQuery(".i18n-en .views-exposed-form .form-item-field-destino-target-id select option[value='All']").text("All the destinations");    
+
+});
 
 // Controlled by buttons
 var $controlledByButtons = $('#controlledByButtons');
