66aa2e9bc3262db9aee488fcb6d8baa55b661215
Keylor Mora <keylor@orbelink.com>
2015-06-05 09:43:32 -0600

    form

--- sites/all/themes/treetopstravel/template.php
+++ sites/all/themes/treetopstravel/template.php
@@ -139,8 +139,14 @@ function treetopstravel_breadcrumb($variables) {
  */
 function treetopstravel_form_alter(&$form, &$form_state, $form_id) {
 
-  if($form_id == "webform_client_form_7" || $form_id == "webform_client_form_18" /*|| $form_id == "webform_client_form_98" || $form_id == "webform_client_form_101" || $form_id == "webform_client_form_104" || $form_id == "webform_client_form_102" || $form_id == "webform_client_form_105" || $form_id == "webform_client_form_106"*/){
-    $form['#attributes']['class'][] = 'grid-1000 form-viaje-a-su-medida';
+  if($form_id == "webform_client_form_7" || $form_id == "webform_client_form_18" ){
+    $form['#attributes']['class'][] = 'form-viaje-a-su-medida';
+    //agregamos la libreria de datepicker para el formulario
+    $form['#after_build'] = array('custom_form_uidatepicker');
+  }
+
+  if($form_id == "webform_client_form_7" || $form_id == "webform_client_form_18" || $form_id == "webform_client_form_98" || $form_id == "webform_client_form_101" || $form_id == "webform_client_form_104" || $form_id == "webform_client_form_102" || $form_id == "webform_client_form_105" || $form_id == "webform_client_form_106"){
+    $form['#attributes']['class'][] = 'formulario-estilo-general';
     //agregamos la libreria de datepicker para el formulario
     $form['#after_build'] = array('custom_form_uidatepicker');
   }
